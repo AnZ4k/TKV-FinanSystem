3f63c675f2f7d8a41bf7978e28de02a5bf7c9c2d
anzak <anzak@AnZ.local>
2021-07-25 21:06:10 -0400

    started a backend development

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, sessions, url_for, redirect, url_for
 
 app = Flask(__name__)
+session = {}
 
 @app.route('/')
 def bar() -> None:
-    return render_template('login.html')
+    if not 'session' in session:
+        return redirect(url_for('login'))
+    return render_template('home.html')
 
+@app.route('/login')
+def login() -> None:
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(port=60000)
